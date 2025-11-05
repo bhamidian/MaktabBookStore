8fff8b8e3c2f735b43ef958423ea9855dd42758a
abolfazl <abolfazlhamidian9@gmail.com>
2025-11-05 15:36:03 +0330

    AddPictures

--- src/02-Services/MaktabBookStore.Services/obj/Debug/net9.0/MaktabBookStore.Services.AssemblyInfo.cs
+++ src/02-Services/MaktabBookStore.Services/obj/Debug/net9.0/MaktabBookStore.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaktabBookStore.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c96e4f2619cb286c401287914f42ff85e5c91a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c7750319bf60f0f6e6678b78eeb7eb88d3a38c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaktabBookStore.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaktabBookStore.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
